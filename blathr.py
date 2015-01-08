1efcefd6588e9cdd2732cc206961f6518e610b9e
samsheets <sam.sheets@gmail.com>
2015-01-07 16:28:41 -0800

    name change! also timestamps

--- blathr.py
+++ blathr.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    db = sqlite3.connect('twit.sqlite')
+    db = sqlite3.connect('blathr.sqlite')
     c = db.cursor()
-    c.execute("select * from tweets")
+    c.execute("select * from posts order by timestamp desc")
     out = ''
-    for tweet in c.fetchall():
-        out += '<p>' + tweet[1] + '</p><hr>'
+    for post in c.fetchall():
+        out += '<p>' + post[1] + '</p><hr>'
     db.close()
     return render_template('template.html', posts=out)
     
 @app.route('/', methods=['POST'])
 def post():
     text = request.form['text']
-    db = sqlite3.connect('twit.sqlite')
+    db = sqlite3.connect('blathr.sqlite')
     c = db.cursor()
-    c.execute("insert into tweets values (1, ?, 1)", [text])
+    c.execute("insert into posts (userid, text) values (1, ?)", [text])
     db.commit()
     db.close()
     return redirect('/', code = 302)
--- makedb.py
+++ makedb.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-db = sqlite3.connect('twit.sqlite')
+db = sqlite3.connect('blathr.sqlite')
 c = db.cursor()
-c.execute("""create table tweets (userid varchar(30), text varchar(255), timestamp varchar(30))""")
+c.execute("""create table posts (userid varchar(30), text varchar(255), timestamp datetime default current_timestamp)""")
 db.commit()
-c.execute("""insert into tweets values (1, "hello", 1)""")
+c.execute("""insert into posts values (1, "hello", 1)""")
 db.commit()
